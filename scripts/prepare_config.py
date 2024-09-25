6a2be389bb2c8c74d2e559227ea2a43a7920c93b
Biraj <biraj.patel@getadapt.ai>
2024-09-25 21:45:47 +0530

    chore: fix prepare_config.py

--- scripts/prepare_config.py
+++ scripts/prepare_config.py
@@ -1,7 +1,9 @@
 """
-This script is used in user-data-client.sh script to update the values of config.toml file which the nomad-vector-logger will use to generate the vector config file.
+This script is used in user-data-client.sh script to update the values of nomad-vector-logger.toml.tpl
+file which the nomad-vector-logger will use to generate the vector config file.
 """
 
+import os
 import sys
 import tomli
 import tomli_w
@@ -9,7 +11,7 @@ import tomli_w
 override = {"loki": {}}
 
 for arg in sys.argv[1:]:
-    pair = arg.split("=")
+    pair = arg.split("=", maxsplit=1)
     if len(pair) != 2:
         print("error: invalid argument", arg)
         exit(1)
@@ -22,9 +24,10 @@ for arg in sys.argv[1:]:
     else:
         override[key] = value
 
+CONFIG_FILE = "./dev/nomad-vector-logger.toml.tpl"
 
 # Read the config.sample.toml file
-with open("config.sample.toml", "rb") as f:
+with open(CONFIG_FILE, "rb") as f:
     config = tomli.load(f)
 
     # update the config with the values from the command line arguments
@@ -39,8 +42,24 @@ with open("config.sample.toml", "rb") as f:
     for key, value in override.items():
         config["app"][key] = value
 
-with open("config.toml", "wb") as f:
-    tomli_w.dump(config, f)
+# backup the original config file
+os.rename(CONFIG_FILE, f"{CONFIG_FILE}.bak")
+
+# write to the ori
+with open(CONFIG_FILE, "w") as f:
+    """
+    # the following lines need to be like this only
+    
+    vector_config_dir = "{{ env "NOMAD_ALLOC_DIR" }}/vector_gen_configs"
+    extra_templates_dir = "{{ env "NOMAD_TASK_DIR" }}/static/"
+
+
+    but the generated config.toml will have the quotations escaped
+    so we need to replace them with double quotes
+    """
+
+    config_str = tomli_w.dumps(config).replace('\\"', '"')
+    f.write(config_str)
     print("👍 config.toml generated")
 
 # this generated config.toml will then be used by nomad-vector-logger program
